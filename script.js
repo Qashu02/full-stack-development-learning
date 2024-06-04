b4d6b273c2c857f307b9f803914846ba11564051
Qashu02 <hafizqasim5566@gmail.com>
2024-06-04 07:49:59 -0700

    practice 3

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-// practice question 1
-let age=23;
-age+=2;
-let eng=23;
-let urdu =34;
-let sci=43;
-let avg=(eng+urdu+sci)/2;
-console.log("average is ",avg);
+let name="qasim";
+console.log(name[0]);
+console.log(name[4]);
+
+let s="apna"
+s=s+" college";
+console.log(s);
+console.log(s.length);
+// empty s length=0 
+let empty="";
+console.log(empty.length);
+
+let space=" ";
+console.log(space.length);
