b44981d20f236d4639287c49221695eb16a8c0ba
Qashu02 <hafizqasim5566@gmail.com>
2024-06-04 22:18:09 -0700

    last digits same

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-let str= prompt("Enter the string");
-if(str[0]==='a' && str.length===3){
-    alert("This is a good String");
-
-}
-else{
-    alert("not a good string");
+// last digit same number
+let num1=prompt("Enter First number: ");
+let num2=prompt("Enter second number: ");
+if (num1[num1.length-1] ===num2[num2.length-1]){
+    alert("Good number")
 }
