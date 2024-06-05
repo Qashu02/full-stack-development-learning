3515d8269ae6bb05bcff08b9e830a9182ae4cce2
Qashu02 <hafizqasim5566@gmail.com>
2024-06-04 21:50:51 -0700

    If else if practice

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-let color=prompt("what is the color of light?");
-if(color==="red"){
-    alert("stop");
-}
-else if(color==="yellow"){
-    alert("start");
-}
 
-else if(color==="green"){
-    alert("go");
+let size=prompt("Enter the size of popcorn you want");
+if(size==="xl"){
+    console.log("Price is 230 Rs")
+}
+else if(size==="small"){
+    console.log("Price is 50rs")
+}
+else if(size==="medium"){
+    console.log("Price is 100rs")
+}
+else if(size==="large"){
+    console.log("Price is 250rs")
+}
+else if(size==="xxl"){
+    console.log("Price is 299rs")
 }
