a703ad6597b50dc7504f215baabe774693befcfc
Qashu02 <hafizqasim5566@gmail.com>
2024-06-04 21:55:16 -0700

    checking good string or not

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
+let str= prompt("Enter the string");
+if(str[0]==='a' && str.length===3){
+    alert("This is a good String");
 
-let size=prompt("Enter the size of popcorn you want");
-if(size==="xl"){
-    console.log("Price is 230 Rs")
 }
-else if(size==="small"){
-    console.log("Price is 50rs")
-}
-else if(size==="medium"){
-    console.log("Price is 100rs")
-}
-else if(size==="large"){
-    console.log("Price is 250rs")
-}
-else if(size==="xxl"){
-    console.log("Price is 299rs")
+else{
+    alert("not a good string");
 }
