e002c63f28230222137097d10604955043bc6f43
Qashu02 <hafizqasim5566@gmail.com>
2024-06-05 21:49:25 -0700

    js methods trim & upper case

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-// program to find the largest among three numbers
-
-// take input from the user
-const num1 = parseFloat(prompt("Enter first number: "));
-const num2 = parseFloat(prompt("Enter second number: "));
-const num3 = parseFloat(prompt("Enter third number: "));
-
-
-// check the condition
-if(num1 >= num2 && num1 >= num3) {
- alert("First is largest")
-}
-else if (num2 >= num1 && num2 >= num3) {
- alert("second is largest");
-}
-else {
-   alert("Third is largest");
-}
-
+let msg="help";
+msg=msg.trim().toUpperCase();
+alert(msg);
