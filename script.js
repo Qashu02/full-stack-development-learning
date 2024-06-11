a7587efab1d0381500e825c5cafd92d0f4653510
Qashu02 <hafizqasim5566@gmail.com>
2024-06-10 23:41:58 -0700

    last n elements of an array

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// . Write a JavaScript program to get the first n elements of an array.
+// . Write a JavaScript program to get the last n elements of an array.
 let num=[1,3,4,6];
 let n=3;
-alert(num.splice(0,3));
+alert(num.slice(1,4));
