8668b7381cbbc52e535e8fbb374db627c62746a2
qashu02 <hafizqasim5566@gmail.com>
2024-07-19 21:13:34 +0500

    functions in js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Write a JavaScript program to check if an element exists in an array or not
-let nums=[1,4,43,5,3,5];
-let item=3;
-if(nums.indexOf(item)!=-1){
-    alert("Element is exist");
-}
-else{
-    alert("Element is not exist");
-}
