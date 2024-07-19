8710b07dd24559b031b62f6a15cf0f85d0d0d09e
qashu02 <hafizqasim5566@gmail.com>
2024-07-19 22:47:23 +0500

    set interval time out function

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-let sqr= (n) => n*n;
-console.log(sqr(2));
+//  function that print hello world 5 times in 2 sec interval
+
+let id=setInterval(function(){
+    console.log("Hello world")
+},2000);
+
+setTimeout(()=>{
+    
+    clearInterval(id);
+    console.log("5 Times completed")
+},10000);
