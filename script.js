eae483741b163a3f417a2351781fcd9d0abe9a26
qashu02 <hafizqasim5566@gmail.com>
2024-07-21 08:14:09 +0500

    every and Some method

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let arr=[{name:"qasim"},
-    {name:"ali"},
-    {name:"asad"},
-];
-arr.forEach(element => {
-    console.log(element.name);
+// checking element is multiple of 10 or not
+
+let arr=[10,240,204540,250]
+// every method used to check the all condition is full filled in an array if yes give true, same as AND operator and Some function is opposite to it 
+let ans=arr.every((el)=>{    
+    return el%10==0;
 });
+console.log(ans)
