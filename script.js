cd7c206960ec9616ec0a44b92cdf72de654416a4
qashu02 <hafizqasim5566@gmail.com>
2024-07-21 09:13:58 +0500

    spread in js

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-// Square and sum the array elements using the arrow function and then find the average of the array.
-let arr=[12,4,42,44,6];
-//  map function used to map the array and create new array and store data of existing array after funtions
-let square=arr.map((el)=>
-{
-return el*el;
-})
-console.log(square);
-let sum= arr.reduce((res,el)=>{
-    return res+el;
-});
-console.log(sum);
-
- let avg= sum/arr.length;
- console.log(avg);
+//  ... this is spread function that used to create the syntax smaller
+let arr=[..."hello"];
+let ans=arr.map(
+    (el)=>{
+        return el.toUpperCase();
+    }
+)
+console.log(ans);
