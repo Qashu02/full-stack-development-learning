cf443e2e578a1d35c062c94b52684dbd9eac7f28
qashu02 <hafizqasim5566@gmail.com>
2024-07-21 09:20:47 +0500

    destructuring in js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-//  ... this is spread function that used to create the syntax smaller
-let arr=[..."hello"];
-let ans=arr.map(
-    (el)=>{
-        return el.toUpperCase();
-    }
-)
-console.log(ans);
+let arr=[ "ali", "saad" ,"usama"];
+//  destructuring is used when we want to create multiple variable and assign the balue to in it 
+// spread is used to take the copy of the data and store in new array 
+let [name1,...name2]=arr;
+console.log(name1);
+console.log(name2);
