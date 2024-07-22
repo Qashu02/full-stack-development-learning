528e40d623ac5bf2208c55e34a7900b49d9103b6
qashu02 <hafizqasim5566@gmail.com>
2024-07-22 22:36:07 +0500

    Random Rgb generator

--- script.js
+++ script.js
@@ -1,37 +1,20 @@
-// Create a new input and button element on the page using JavaScript only. Set the
-// text of button to "Click me'
-let body=document.querySelector("body");
+// Random Rgb generator app 
+let btn=document.querySelector("button");
+let h3=document.querySelector("h3");
+let div=document.querySelector("div");
 
-let btn=document.createElement("button");
-let input=document.createElement("input");
-body.prepend(input);
+Randomcolor=()=>{
+    let red=Math.floor(Math.random()*255);
+    let Green=Math.floor(Math.random()*255);
+    let blue=Math.floor(Math.random()*255);
+    let color=`rgb(${red} , ${Green}, ${blue})`;
+    return color;
+}
+btn.addEventListener('click',function(){
+    let random=Randomcolor();
+    h3.innerText=random;
+    h3.style.color=random;
+    div.style.backgroundColor=random;
+})
 
-btn.innerText="CLick me";
-body.append(btn)
-
-// Qs2. Add following attributes to the element :
-// Change placeholder value of input to "username"
-// // Change the id of button to "btn'
-
-btn.setAttribute("id","btn");
-input.setAttribute("placeholder","username");
-
-// Qs3. Access the btn using the querySelector and button id. Change the button background
-// color to blue and text color to white.
-
-let btn_id=document.querySelector("#btn");
-
-btn_id.style.color="purple"
-
-// Qs4. Create an hl element on the page and set its text to "DOM Practice" underlined.
-// Change its color to purple.
-
-let h1=document.createElement("h1");
-h1.innerText="Dom Pratice";
-h1.style.color="purple"
-body.prepend(h1);
-
-p=document.createElement("p");
-p.innerText="Hard work";
-p.style.color="red";
-body.append(p);
+  
