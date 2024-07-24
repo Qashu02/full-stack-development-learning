bf986036ec265bebbb2f4a4a56307045a6129c70
qashu02 <hafizqasim5566@gmail.com>
2024-07-24 08:38:53 +0500

    promises and call back hell

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-let btn =document.querySelector("button");
-let ul=document.querySelector("ul");
-btn.addEventListener('click', function(){
-    let input=document.querySelector("input");
-   let item=document.createElement("li");
-   
-let del=document.createElement("button");
+// Promises is an object used to process the request asynschroneosly means that if an action based on first action it 
+// reslove the isse . it gives us two state reslove , reject 
+// sometimes call back hell occurs which means that we use call back to perform asychroneously but 
+//  the code is not understandable so we use promises to reslove the issue and make our program to understand
 
+let h1= document.querySelector("h1");
 
-   item.innerText=input.value;
-del.innerText="Delete";
-del.classList.add("delete");
-item.append(del);
+function ColorChange(color,delay){
+    // function will return the promise value
+   return new Promise((resolove,rejected)=>{
+        setTimeout(()=>{
+            h1.style.color=color;
+            resolove(`:color changed`);
+        },delay);
+    });
+};
 
-
-   ul.appendChild(item);
-   input.value="";
-
+ColorChange("red", 1000)
+//  then keyword is used when our promise is successfull what the next function will be executed?
+.then((result)=>{
+    console.log(`successful to red ${result}`);
+    // it also return the new promise that await for the confrmation of the first function to be executed
+    return ColorChange("blue",1000);
 })
-// event Delegation is used when bubbling is held to parent when triggered
-// bubling is the concept that if the event listener is addes to the parent then it will also bre on child 
-ul.addEventListener("click",function(event){
-    if(event.target.nodeName == "BUTTON"){
-        let lists=event.target.parentElement;
-        lists.remove()
-    }
+.then((result)=>{
+    console.log(`successful to blue ${result}`);
+    return ColorChange("green",1000);
+
+}).then((result)=>{
+    console.log(`successful to green ${result}`);
 })
+// if an error occur and failure displayed error then we use to resolve it by catch keyword 
