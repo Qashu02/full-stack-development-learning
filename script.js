6f0f98b6337cfdb6f7571d0a6653368b6a5cddbb
qashu02 <hafizqasim5566@gmail.com>
2024-07-24 23:20:08 +0500

    API fetching with promise chaining

--- script.js
+++ script.js
@@ -1,33 +1,20 @@
-//  async function is the better way to make the program asynchronus it returns by default a promise
-//  either its reject or resolve 
+let url ="https://catfact.ninja/fact";
 
-function GenerateNum (delay){
-return new Promise((resolve, reject )=>{
-  setTimeout(() => {
-    let num =  Math.floor(Math.random()*10)+1;
-    console.log(num);
-    if(num<5){
-      console.log("No traffic");
-      resolve();
-    }else{
-      console.log("high traffic")
-      reject();
-    }
-  }, delay);
+// api fetching  with help of promises
+
+fetch(url)
+.then((res)=>{
+ 
+  return res.json();
+}).then((data)=>{
+  console.log(data.fact);
+  return fetch(url)
+  .then((res)=>{
+    return res.json();
+
+  }).then((data2)=>{
+    console.log(data2.fact);
+  })
+}).catch((fail)=>{
+  console.log("Error-",fail);
 })
-}
-// await funcrion is used in the async it block the surrounding function to execute vefore the existing one executed
-   async function TrafficController(){
-//  error handling through Try and catch in asysnc function 
-try{
-
-    await GenerateNum(1000);
-    await GenerateNum(1000);
-     GenerateNum(1000);
-}catch{
-    console.log("error occur")
-}
-   }
-
-   TrafficController();
-
