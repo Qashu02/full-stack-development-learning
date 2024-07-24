e349d05ddaf99b302e8b31e2d66efb75f8d62b75
qashu02 <hafizqasim5566@gmail.com>
2024-07-24 09:23:53 +0500

    await and async function

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-// Promises is an object used to process the request asynschroneosly means that if an action based on first action it 
-// reslove the isse . it gives us two state reslove , reject 
-// sometimes call back hell occurs which means that we use call back to perform asychroneously but 
-//  the code is not understandable so we use promises to reslove the issue and make our program to understand
+//  async function is the better way to make the program asynchronus it returns by default a promise
+//  either its reject or resolve 
 
-let h1= document.querySelector("h1");
-
-function ColorChange(color,delay){
-    // function will return the promise value
-   return new Promise((resolove,rejected)=>{
-        setTimeout(()=>{
-            h1.style.color=color;
-            resolove(`:color changed`);
-        },delay);
-    });
-};
-
-ColorChange("red", 1000)
-//  then keyword is used when our promise is successfull what the next function will be executed?
-.then((result)=>{
-    console.log(`successful to red ${result}`);
-    // it also return the new promise that await for the confrmation of the first function to be executed
-    return ColorChange("blue",1000);
+function GenerateNum (delay){
+return new Promise((resolve, reject )=>{
+  setTimeout(() => {
+    let num =  Math.floor(Math.random()*10)+1;
+    console.log(num);
+    if(num<5){
+      console.log("No traffic");
+      resolve();
+    }else{
+      console.log("high traffic")
+      reject();
+    }
+  }, delay);
 })
-.then((result)=>{
-    console.log(`successful to blue ${result}`);
-    return ColorChange("green",1000);
+}
+// await funcrion is used in the async it block the surrounding function to execute vefore the existing one executed
+   async function TrafficController(){
+//  error handling through Try and catch in asysnc function 
+try{
 
-}).then((result)=>{
-    console.log(`successful to green ${result}`);
-})
-// if an error occur and failure displayed error then we use to resolve it by catch keyword 
+    await GenerateNum(1000);
+    await GenerateNum(1000);
+     GenerateNum(1000);
+}catch{
+    console.log("error occur")
+}
+   }
+
+   TrafficController();
+
